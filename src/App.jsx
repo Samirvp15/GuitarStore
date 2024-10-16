07b9e8976056e30c0eb232ba4aaad7a8fa9c18d4
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-16 14:20:59 -0500

    add Db.js and props to GuitarState

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
+import { useState } from 'react';
 
-import Header from "./components/Header"
-import Guitar from "./components/Guitar"
-
+import { db } from './data/db';
+import Header from './components/Header';
+import Guitar from './components/Guitar';
 
 function App() {
+  //State
+    const [data, setData] = useState(db)
+
 
   return (
     <>
-    
-    <Header />
+      <Header />
 
+      <main className='container-xl mt-5'>
+        <h2 className='text-center'>Nuestra Colección</h2>
 
+        <div className='row mt-5'>
+         
+          {data.map((guitar) => 
+          <Guitar
+            
+          key={guitar.id}
+            guitar = {guitar}
 
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+          
+          />
+          
+          )}
 
-        <div className="row mt-5">
-
-        <Guitar/>
-        
-        <Guitar/>
+          
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+      <footer className='bg-dark mt-5 py-5'>
+        <div className='container-xl'>
+          <p className='text-white text-center fs-4 mt-4 m-md-0'>
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
-
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
